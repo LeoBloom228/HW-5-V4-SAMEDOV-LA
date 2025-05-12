94d28a0a72024a35be41a0df77501c3c151c9365
samedov.l <samedov.leon@outlook.com>
2025-05-12 16:57:56 +0300

    Задание 4 в рамках ДЗ 5. Значения a и b вводим с клавиатуры: 0 и 0.99 соответственно.

--- Homework-5/file1.cpp
+++ Homework-5/file1.cpp
@@ -57,29 +57,89 @@
 
 //Task 3
 
+//#include <iostream>
+//#include <cmath>
+
+//int main()
+
+//{
+    //double epsilon;
+
+    //std::cout << "Input epsilon value: ";
+    //std::cin >> epsilon;
+
+    //double sum = 0.0;
+    //double term = 1.0;
+    //int i = 1;
+
+    //while (std::abs(term) >= epsilon) 
+    //{
+        //sum += term;
+        //term = 1.0 / (i * (i + 1));
+        //i++;
+    //}
+
+    //std::cout << "The sum with a given epsilon value which is: " << epsilon << " equals " << sum << std::endl;
+
+    //return 0;
+//}
+
+//Task 4
+
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
-int main()
-
+// Вспомогательная функция для вычисления члена ряда
+double term(int n, double x) 
 {
-    double epsilon;
+    return std::pow(x, 2 * n + 1) / ((2 * n + 1) * std::pow(2, n));
+}
 
-    std::cout << "Input epsilon value: ";
-    std::cin >> epsilon;
-
+// Функция для вычисления суммы ряда с заданной точностью
+double f(double x, double epsilon, int& n) 
+{
     double sum = 0.0;
-    double term = 1.0;
-    int i = 1;
+    double current_term = term(0, x);
+    n = 0;
 
-    while (std::abs(term) >= epsilon) 
+    while (std::abs(current_term) >= epsilon) 
     {
-        sum += term;
-        term = 1.0 / (i * (i + 1));
-        i++;
+        sum += current_term;
+        n++;
+        current_term = term(n, x);
     }
 
-    std::cout << "The sum with a given epsilon value which is: " << epsilon << " equals " << sum << std::endl;
+    return sum;
+}
+
+int main() 
+{
+       
+        double a, b, epsilon;
+
+        std::cout << "Input a (it should be 0): ";
+        std::cin >> a;
+
+        std::cout << "Input b (it should be 0.99): ";
+        std::cin >> b;
+
+        std::cout << "Input epsilon: ";
+        std::cin >> epsilon;
+
+        double h = 0.1;
+        double x = a;
+        int i = 1;
+
+        std::cout << std::setw(10) << "No." << std::setw(10) << "x" << std::setw(20) << "F(x)" << std::setw(10) << "n" << std::endl;
+
+        while (x <= b) {
+            int n;
+            double result = f(x, epsilon, n);
+            std::cout << std::setw(10) << i << std::setw(10) << x << std::setw(20) << result << std::setw(10) << n << std::endl;
+            x += h;
+            i++;
+        }
 
     return 0;
 }
