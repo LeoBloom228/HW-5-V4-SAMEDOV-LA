612b6540040921f1c602d09b4dd7540567d8cab1
samedov.l <samedov.leon@outlook.com>
2025-05-12 15:02:44 +0300

    Задание 2 в рамках ДЗ 5.

--- Homework-5/file1.cpp
+++ Homework-5/file1.cpp
@@ -1,26 +1,56 @@
 //Task 1
 
+//#include <iostream>
+//#include <cmath>
+//#include <iomanip>
+
+//int main()
+//{
+	//int n;
+
+	//std::cout << "Input n: ";
+	//std::cin >> n;
+
+	//double sum = 0.0;
+
+	//for (int i = 1; i <= n; ++i) 
+	//{
+		//sum += 1.0 / (i * i);
+	//}
+
+	//std::cout << "Sum equals: " << sum << std::endl;
+
+
+
+	//return 0;
+//}
+
+//Task 2
+
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 
-int main()
+
+int main() 
 {
-	int n;
+    int k;
+    double x;
 
-	std::cout << "Input n: ";
-	std::cin >> n;
+    std::cout << "Input k: ";
+    std::cin >> k;
 
-	double sum = 0.0;
+    std::cout << "Input x: ";
+    std::cin >> x;
 
-	for (int i = 1; i <= n; ++i) 
-	{
-		sum += 1.0 / (i * i);
-	}
+    double sum = 0.0;
 
-	std::cout << "Sum equals: " << sum << std::endl;
+    for (int n = 1; n <= k; ++n) 
+    {
+        sum += std::pow(x, 2 * (n - 1)) / std::pow(2 + 4 * (n - 1), 2);
+    }
 
+    std::cout << "Sum equals: " << sum << std::endl;
 
+    return 0;
 
-	return 0;
 }
